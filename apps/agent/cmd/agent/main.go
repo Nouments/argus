084e1351da91b720fe898352e1fc2951e435c47b
Nouments <nomentsua@gmail.com>
2026-08-25 20:09:00 +0300

    host config on agent

--- apps/agent/cmd/agent/main.go
+++ apps/agent/cmd/agent/main.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/Nouments/argus/apps/agent/internal/config"
+)
+
+func main() {
+	path := config.ExportPathByOS("linux")
+	fmt.Println(path)
+}
--- apps/agent/internal/config/config.go
+++ apps/agent/internal/config/config.go
@@ -1,1 +1,15 @@
 package config
+
+import (
+	"strings"
+)
+
+
+func ExportPathByOS(Os string) string {
+	if strings.ToLower(Os) == "windows"{
+		return `C:\ProgramData\Argus\config\agent.yaml`
+	}
+	return `/etc/argus/config/agent.yaml`
+}
+
+
